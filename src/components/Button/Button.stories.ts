9afdc35e52f8269fc44c276f042ec761fdd5aa80
Ralph Effting <ralfting@gmail.com>
2025-02-18 11:43:03 -0300

    Add chromatic and fix font size

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -1,17 +1,35 @@
 import type { Meta, StoryObj } from "@storybook/react";
-
 import Button from "./Button";
 
+// Configuração do meta
 const meta = {
   component: Button,
+  argTypes: {
+    as: {
+      control: {
+        type: "radio",
+        options: ["button", "a"],
+      },
+      defaultValue: "button",
+    },
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Example = {
+export const ButtonExample = {
   args: {
     variant: "primary",
     children: "My Button",
+    as: "button",
+  },
+} satisfies Story;
+
+export const LinkExample = {
+  args: {
+    variant: "primary",
+    children: "My Link Button",
+    as: "a",
   },
 } satisfies Story;
